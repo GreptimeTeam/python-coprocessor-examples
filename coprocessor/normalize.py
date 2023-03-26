75d08899c1d1811e54c14526f52de6223a404a01
Dennis Zhuang <killme2008@gmail.com>
2023-03-26 11:40:13 +0800

    fix: some examples

--- coprocessor/normalize.py
+++ coprocessor/normalize.py
@@ -2,14 +2,15 @@ import math
 
 def normalize0(x):
     if x is None or math.isnan(x):
-        return 10
-    elif x > 10:
-        return 100
-    elif x < 5:
+        return 0
+    elif x > 5:
         return 5
+    elif x < 0:
+        return 0
     else:
         return x
 
-@coprocessor(args=["number"], sql="select number from numbers limit 20", returns=["value"])
+@coprocessor(args=["number"],
+             sql="select number from numbers limit 10", returns=["value"])
 def normalize(v) -> vector[i64]:
     return [normalize0(x) for x in v]
--- io/args.py
+++ io/args.py
@@ -1,3 +1,5 @@
-@coprocessor(args=["number", "number"], returns=["value"])
+@coprocessor(args=["a", "b"],
+             returns=["value"],
+             sql="select number as a,number as b from numbers limit 10")
 def add_vectors(a, b) -> vector[i64]:
     return a + b
